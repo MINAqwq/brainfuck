2a714e9064164e2b9a37859fdcea1ab6eafc6ad7
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2023-09-22 17:48:58 +0200

    finished interpreter

--- src/interpreter.s
+++ src/interpreter.s
@@ -2,6 +2,10 @@
 
 .file "interpreter.s"
 
+.data
+str_unknown_synbol:
+.asciz "error: unknown symbol in code\n"
+
 .text
 .global interpret
 .type interpret, @function
@@ -95,22 +99,200 @@ interpret:
 	jmp .L08
 
 .L06:
+	/* [ */
+	cmpb $91, (%rdi)
+	jne .L07
+	mov %rsp, %rcx
+	add -16(%rbp), %rcx
+
+	/* if value at mem index is not zero enter loop */
+	cmpb $0, (%rcx)
+	je .skip_loop
+	
+	call enter_loop
+	jmp .L08
+
+.skip_loop:
+	/* go to next ] */
+	inc %rdi
+	cmpb $93, (%rdi)
+	jne .skip_loop
+	jmp .L08
+
+.L07:
+	/* ] */
+	cmpb $93, (%rdi)
+	jne .L09
+	mov %rsp, %rcx
+	add -16(%rbp), %rcx
+
+	/* if value at mem index is zero leave loop, otherwise jump */
+	cmpb $0, (%rcx)
+	je .leave
+	call pop_loop
+	jmp .L08
+
+.leave:
+	call leave_loop
 
 .L08:
 	inc %rdi
 	jmp .LOOP_START
+
+
+.L09:
+	/* if newline, ignore */
+	cmpb $10, (%rdi)
+	je .L08
+
+	/* if tab, ignore */
+	cmpb $9, (%rdi)
+	je .L08
+
+	/* if space, ignore */
+	cmpb $32, (%rdi)
+	je .L08
+
+	/* unknown symbol */
+	lea str_unknown_synbol(%rip), %rdi
+	call print
+
+	mov $1, %rdi
+	call exit
+	ret
+
 .LOOP_END:
 	/* end interpret */
-	
 	add $4136, %rsp
 	pop %rbp
 	ret
 
 .global enter_loop
 .type enter_loop, @function
-/* rdi: addr to store | rsi: call stack ptr */
+/* rdi: addr to store */
 enter_loop:
-	cmp $0, %rsi
-	je .MMAP
-.MMAP:
+	cmpq $0, -32(%rbp)
+	jne .resize
+
+.init_alloc:
+	push %rdi
+	mov $8, %rdi
+	call mem_alloc
+
+	/* save callstack pointer on stack */
+	mov %rax, -24(%rbp)
+
+	/* increment callstack size (should be 1 now) */
+	incq -32(%rbp)
+
+	pop %rdi
+
+	/* save address on callstack */
+	mov %rdi, (%rax)
+	ret
+
+.resize:
+	push %rdi
+	push %rsi
+	push %rdx
+
+	/* callstack addr */
+	mov -24(%rbp), %rdi
+
+	/* multiply by array lenght for old size */
+	mov -32(%rbp), %rsi
+	mov $8, %rax
+	mul %rsi
+	mov %rax, %rsi
+
+	/* new size */
+	mov %rsi, %rdx
+	add $8, %rdx
+
+	call mem_realloc
+
+	incq -32(%rbp)
+
+	/* index with old size */
+	mov %rdi, %rcx
+	add %rsi, %rcx
+
+	pop %rdx
+	pop %rsi
+	pop %rdi
+
+	/* save addr on callstack */
+	mov %rdi, (%rcx)
 	ret	
+
+/* its not really a pop, because it wont change the callstack */
+.global pop_loop
+.type pop_loop, @function
+pop_loop:
+	/* save callstack pointer in rbx and size in rcx */
+	mov -24(%rbp), %rbx
+	mov -32(%rbp), %rcx
+
+	/* multiply size by 8 */
+	mov $8, %rax
+	dec %rcx
+	mul %rcx
+
+	/* index */
+	add %rax, %rbx
+
+	/* write addr to rdi */
+	movq (%rbx), %rdi
+	ret
+
+.global leave_loop
+.type leave_loop, @function
+leave_loop:
+	push %rdi
+	push %rsi
+
+	/* save callstack pointer in rdi and size in rcx */
+	mov -24(%rbp), %rdi
+	mov -32(%rbp), %rcx
+
+	cmp $1, %rcx
+	jne .shrink
+
+.delete:
+	mov $8, %rsi
+	call mem_free
+
+	/* null out callstack */
+	movq $0, -24(%rbp)
+	decq -32(%rbp)
+
+	pop %rsi
+	pop %rdi
+
+	ret
+
+.shrink:
+	push %rdx
+
+	/* size * addr_len */
+	mov $8, %rax
+	mul %rcx
+
+	/* save old size in rsi */
+	mov %rax, %rsi
+	
+	/* save new size in rdx */
+	sub $8, %rax
+	mov %rax, %rdx
+
+	/* shrink callstack by 8 */
+	call mem_realloc
+
+	/* decrement size */
+	decq -32(%rbp)
+
+	pop %rdx
+	pop %rsi
+	pop %rdi
+
+	ret
